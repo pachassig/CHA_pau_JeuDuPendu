4e6fb186b36a76bec95c63518ac52f9a6b8e319b
pachassig <pchassignol@gamingcampus.in>
2025-10-29 19:17:34 +0100

    correct affiche_pendu

--- JeuDuPendu/jeu.c
+++ JeuDuPendu/jeu.c
@@ -4,71 +4,47 @@
 #include <stdbool.h>
 
 void affiche_pendu(int nombre_erreurs) {
+	printf(" +---+\n");
+	printf(" |   |\n");
 	switch (nombre_erreurs) {
 		case 0 : 
-			printf(" +---+\n");
-			printf(" |   |\n");
 			printf("     |\n");
 			printf("     |\n");
 			printf("     |\n");
-			printf("     |\n");
-			printf("========\n");
 			break;
 		case 1:
-			printf(" +---+\n");
-			printf(" |   |\n");
 			printf(" O   |\n");
 			printf("     |\n");
 			printf("     |\n");
-			printf("     |\n");
-			printf("========\n");
 			break;
 		case 2:
-			printf(" +---+\n");
-			printf(" |   |\n");
 			printf(" O   |\n");
 			printf(" |   |\n");
 			printf("     |\n");
-			printf("     |\n");
-			printf("========\n");
 			break;
 		case 3:
-			printf(" +---+\n");
-			printf(" |   |\n");
 			printf(" O   |\n");
 			printf(" |\  |\n");
 			printf("     |\n");
-			printf("     |\n");
-			printf("========\n");
 			break;
 		case 4:
-			printf(" +---+\n");
-			printf(" |   |\n");
 			printf(" O   |\n");
 			printf("/|\  |\n");
 			printf("     |\n");
-			printf("     |\n");
-			printf("========\n");
 			break;
 		case 5:
-			printf(" +---+\n");
-			printf(" |   |\n");
 			printf(" O   |\n");
 			printf("/|\  |\n");
 			printf("  \  |\n");
-			printf("     |\n");
-			printf("========\n");
 			break;
 		case 6:
-			printf(" +---+\n");
-			printf(" |   |\n");
 			printf(" O   |\n");
 			printf("/|\  |\n");
 			printf("/ \  |\n");
-			printf("     |\n");
-			printf("========\n");
 			break;
 	}
+	printf("     |\n");
+	printf("========\n");
 }
 
 void affiche_mot() {
